71e5a637fccfa9a3dd94b2ecc0a41e89c39247ce
Hossein Hassanzadeh <hosseinhzr@gmail.com>
2020-07-31 18:29:38 +0300

    Added Client UI application

---
+++ projects/client-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bet-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client-ui';
+}
