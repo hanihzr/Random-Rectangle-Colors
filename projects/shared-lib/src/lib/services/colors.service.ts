c8feae66b732fcebdefcc5fbcb843a0bedc5edaa
Hossein Hassanzadeh <hosseinhzr@gmail.com>
2020-08-01 06:01:41 +0300

    Finished Rectangle component

--- projects/shared-lib/src/lib/services/colors.service.ts
+++ projects/shared-lib/src/lib/services/colors.service.ts
@@ -4,16 +4,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { IRandomColor } from '../models';
+import { SharedLibModule } from '../shared-lib.module';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: SharedLibModule,
 })
 export class ColorsService {
   private url: string = 'http://www.colr.org/json/color/random';
 
   constructor(private http: HttpClient) {}
 
-  getColors() {
+  getColors(): Observable<IRandomColor> {
     return this.http.get(this.url) as Observable<IRandomColor>;
   }
 }
--- projects/shared-lib/src/lib/shared-lib.module.ts
+++ projects/shared-lib/src/lib/shared-lib.module.ts
@@ -1,19 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects/src/effects_module';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools/src/instrument';
-import { StoreModule } from '@ngrx/store/src/store_module';
-
-import { RootEffects } from './ngrx/effects/root';
-import { RootReducers } from './ngrx/store/reducers/root';
 
 @NgModule({
   declarations: [],
   imports: [
-    StoreModule.forRoot({ ...RootReducers }),
-    EffectsModule.forRoot(RootEffects),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-    }),
+    HttpClientModule,
+    // StoreModule.forRoot({ ...RootReducers }),
+    // EffectsModule.forRoot(RootEffects),
+    // StoreDevtoolsModule.instrument({
+    //   maxAge: 25,
+    // }),
   ],
   exports: [],
 })
